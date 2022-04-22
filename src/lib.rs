40bb5fe542394fd677cbe6565ff87098403799dd
Nanai Jua <me@n7i.dev>
2022-04-22 16:40:43 +0900

    🚧 impls `From` `TryFrom` for unsigned numbers.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,39 @@
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
 struct Usize<const N: usize>;
+
+macro_rules! try_from_impls {
+    ($($ty:ty)*) => {$(
+        impl<const N: usize> TryFrom<$ty> for Usize<N> {
+            type Error = ();
+
+            fn try_from(value: $ty) -> Result<Self, Self::Error> {
+                if value == N as $ty {
+                    Ok(Usize::<N>)
+                } else {
+                    Err(())
+                }
+            }
+        }
+    )*};
+}
+
+try_from_impls! { u8 u16 u32 u64 u128 usize }
+
+macro_rules! from_impls {
+    ($($ty:ty)*) => {$(
+        impl<const N: usize> From<Usize<N>> for $ty {
+            fn from(_: Usize<N>) -> Self { N as $ty }
+        }
+    )*};
+}
+
+from_impls! { u8 u16 u32 u64 u128 usize }
+
+impl<const N: usize> PartialEq<usize> for Usize<N> {
+    fn eq(&self, other: &usize) -> bool { Usize::<N>::try_from(*other).is_ok() }
+}
+
+impl<const N: usize> PartialEq<Usize<N>> for usize {
+    fn eq(&self, _: &Usize<N>) -> bool { Usize::<N>::try_from(*self).is_ok() }
+}
