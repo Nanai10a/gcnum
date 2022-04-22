c568ccc1b50e0698a40732980c81aeda09e38630
Nanai Jua <me@n7i.dev>
2022-04-22 16:42:47 +0900

    🚧 impls `Serialize` `Deserialize` from `serde` .

--- src/lib.rs
+++ src/lib.rs
@@ -49,3 +49,67 @@ fn check_value() {
     let _: usize = gnum.into();
     let _: Usize<771> = pnum.try_into().unwrap();
 }
+
+// serde impls
+
+use core::fmt;
+
+use serde::de::Visitor;
+use serde::{de, Deserialize, Deserializer, Serialize, Serializer};
+
+impl<const N: usize> Serialize for Usize<N> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where S: Serializer {
+        serializer.serialize_u64(Into::<usize>::into(*self) as u64)
+    }
+}
+
+struct UsizeVisitor<const N: usize>;
+
+macro_rules! into_or_fallback {
+    ($fn:ident($ty:ty) | -> $nfn:ident($nty:ty)) => {
+        fn $fn<E: de::Error>(self, v: $ty) -> Result<Self::Value, E> {
+            if <$ty>::BITS == usize::BITS {
+                TryInto::try_into(v).map_err(|_| {
+                    de::Error::invalid_value(de::Unexpected::Unsigned(v.into()), &self)
+                })
+            } else {
+                self.$nfn(v as $nty)
+            }
+        }
+    };
+    ($fn:ident($ty:ty) |) => {
+        fn $fn<E: de::Error>(self, v: $ty) -> Result<Self::Value, E> {
+            if <$ty>::BITS == usize::BITS {
+                TryInto::try_into(v).map_err(|_| {
+                    de::Error::invalid_value(de::Unexpected::Unsigned(v.into()), &self)
+                })
+            } else {
+                unreachable!();
+            }
+        }
+    };
+}
+
+impl<'de, const N: usize> Visitor<'de> for UsizeVisitor<N> {
+    type Value = Usize<N>;
+
+    into_or_fallback! { visit_u8(u8) |-> visit_u16(u16) }
+
+    into_or_fallback! { visit_u16(u16) |-> visit_u32(u32) }
+
+    into_or_fallback! { visit_u32(u32) |-> visit_u64(u64) }
+
+    into_or_fallback! { visit_u64(u64) | }
+
+    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
+        write!(formatter, "number must be {}", N)
+    }
+}
+
+impl<'de, const N: usize> Deserialize<'de> for Usize<N> {
+    fn deserialize<D>(deserliazer: D) -> Result<Self, D::Error>
+    where D: Deserializer<'de> {
+        deserliazer.deserialize_u8(UsizeVisitor)
+    }
+}
